d952def94da088e0e2780c5158782b481cc5f06b
Jeff Hall <jhall@electrikdevelopment.co.uk>
2018-02-13 18:11:53 +0000

    Update deployment scripts

--- deploy.js
+++ deploy.js
@@ -2,20 +2,21 @@ const fs = require('fs');
 const { exec } = require('child_process');
 require('dotenv').config();
 
+const exportStatement = (name, value) => name && value ? `export ${name}="${value}" && ` : '';
+
 const dockerFile = `
 FROM lambci/lambda:build-nodejs6.10
 
 ADD . .
 
-RUN export aws_access_key_id=${process.env.AWS_ACCESS_KEY_ID} && \\
-export aws_secret_access_key=${process.env.AWS_SECRET_ACCESS_KEY} && \\
-export AWS_ACCESS_KEY_ID=${process.env.AWS_ACCESS_KEY_ID} && \\
-export AWS_SECRET_ACCESS_KEY=${process.env.AWS_SECRET_ACCESS_KEY} && \\
-export SOURCE_S3_BUCKET_NAME="${process.env.SOURCE_S3_BUCKET_NAME}" && \\
-export AWS_REGION='${process.env.AWS_REGION}' && \\
-export SOURCE_S3_WEBSITE="${process.env.SOURCE_S3_BUCKET_URL}"  && \\
-npm install && npm install serverless -g && sls deploy -v
-exit 0
+RUN ${exportStatement('aws_access_key_id', process.env.AWS_ACCESS_KEY_ID)} \\
+${exportStatement('aws_secret_access_key', process.env.AWS_SECRET_ACCESS_KEY)} \\
+${exportStatement('AWS_ACCESS_KEY_ID', process.env.AWS_ACCESS_KEY_ID)} \\
+${exportStatement('AWS_SECRET_ACCESS_KEY', process.env.AWS_SECRET_ACCESS_KEY)} \\
+${exportStatement('AWS_REGION', process.env.AWS_REGION)} \\
+${exportStatement('SOURCE_S3_BUCKET_NAME', process.env.SOURCE_S3_BUCKET_NAME)} \\
+${exportStatement('SOURCE_S3_WEBSITE', process.env.SOURCE_S3_BUCKET_URL)} \\
+npm install && npm install serverless -g && sls deploy -v && exit 0
 `;
 
 
@@ -23,9 +24,8 @@ const createDockerFile = () => new Promise((resolve, reject) => fs.writeFile('./
 	if (err) {
 		reject(err);
 	}
-	console.log('The temp Dockerfile was created! Please wait while we deploy your cdn ...');
+	console.log('The temp Dockerfile was created! Please wait while we deploy your cdn...');
 	resolve();
-            // docker build -t sls-zone-file-cdn . && docker run -it sls-zone-file-cdn
 }));
 
 const removeDockerFile = () => new Promise((resolve, reject) => fs.unlink('./Dockerfile', (err) => {
@@ -37,17 +37,20 @@ const removeDockerFile = () => new Promise((resolve, reject) => fs.unlink('./Doc
 }));
 
 const runDockerFile = () => new Promise((resolve, reject) => {
-	const dockerbuildDeploy = exec('docker build -t sls-zone-file-cdn . && docker run sls-zone-file-cdn');
-    dockerbuildDeploy.stderr.on('data', (data) => {
+	const dockerbuildDeploy = exec('docker build -t sls-zone-file-cdn .');
+	dockerbuildDeploy.stderr.on('data', (data) => {
 		console.error(`stderr: ${data}`);
 	});
-    dockerbuildDeploy.on('close', (code) => {
+  dockerbuildDeploy.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+	dockerbuildDeploy.on('close', (code) => {
 		if (code === 0) {
-			resolve();
-		} else {
-			console.error(`child process exited with code ${code}`);
-			reject();
-		}
+      resolve();
+    } else {
+      console.error(`child process exited with code ${code}`);
+      reject();
+    }
 	});
 });
 
